636c7f7cca2ac667a499f31323e1edfc54eb7b57
jonaproitz <jonaproitz@gmail.com>
2021-09-30 15:13:49 +0200

    Decleare constructor

--- include/Particle.hpp
+++ include/Particle.hpp
@@ -1,8 +1,16 @@
 
+#include <armadillo>
+
 #ifndef __Particle_hpp__
 #define __Partilce_hpp__
 
 class Particle{
+private:
+  // Declaration of variables only accessible from within the class
+
+public:
+  // Declaration of constructor
+    Particle(int charge, double mass, arma::vec position, arma::vec velocity);
 
 };
 
