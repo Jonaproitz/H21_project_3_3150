b95f668317cf94cc8136bc2d4250b84f4623ad1d
jonaproitz <jonaproitz@gmail.com>
2021-09-30 15:21:17 +0200

    Define constructor

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -1,3 +1,10 @@
 
 #include "Particle.hpp"
 
+// Constructor
+Particle::Particle(int charge_in, double mass_in, arma::vec position_in, arma::vec velocity_in){
+    int charge = charge_in;
+    double mass = mass_in;
+    arma::vec position = position_in;
+    arma::vec velocity = velocity_in;
+}
