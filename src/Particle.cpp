694035f25cbd14c83c3c24b056b4150c60e8aae5
jonaproitz <jonaproitz@gmail.com>
2021-10-04 16:04:34 +0200

    Remove decleration from private variables in constructor

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -3,10 +3,10 @@
 
 // Constructor
 Particle::Particle(int charge_in, double mass_in, arma::vec position_in, arma::vec velocity_in){
-    int charge_ = charge_in;
-    double mass_ = mass_in;
-    arma::vec position_ = position_in;
-    arma::vec velocity_ = velocity_in;
+    charge_ = charge_in;
+    mass_ = mass_in;
+    position_ = position_in;
+    velocity_ = velocity_in;
 }
 
 int Particle::charge(){
