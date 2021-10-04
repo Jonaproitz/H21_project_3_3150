3ae7bd1fc33e767d9644d24f5ac0d59806a04448
jonaproitz <jonaproitz@gmail.com>
2021-10-04 15:40:26 +0200

    Call constructor

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 
-#include "Particle.hpp"
 #include "PenningTrap.hpp"
 
 int main(){
-    
+    // Initialize Penning trap.
+    double B0 = 9.65e1, V0 = 9.65e8, d = 1e4;
+    PenningTrap my_trap(B0, V0, d);
+
+
+
     return 0;
 }
