4256e770b686f56d39962c54c7a980c76e2266e5
jonaproitz <jonaproitz@gmail.com>
2021-10-04 14:20:48 +0200

    Add vector to contain particles

--- src/PenningTrap.cpp
+++ src/PenningTrap.cpp
@@ -6,8 +6,9 @@ PenningTrap::PenningTrap(double B0_in, double V0_in, double d_in){
     double B0 = B0_in;
     double V0_in;
     double d = d_in;
+    std::vector<Particle> Particle;
 }
 
 void PenningTrap::add_particle(Particle particle_in){
-
+    
 }
