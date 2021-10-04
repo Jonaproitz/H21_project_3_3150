994cdd758dea7efbfb68bbe01c3626465d3c7719
jonaproitz <jonaproitz@gmail.com>
2021-10-04 13:20:35 +0200

    Create penningtrap class and constructor

---
+++ include/PenningTrap.hpp
@@ -0,0 +1,10 @@
+
+#include "Particle.hpp"
+#include <vector>
+
+class PenningTrap{
+    private:
+
+    public:
+        PenningTrap(double B0, double V0, int d, std::vector<Particle> particles);
+};
