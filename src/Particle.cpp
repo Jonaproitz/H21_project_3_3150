7e5732d967ed9914bd8f2c31349234b17a8e3f1a
jonaproitz <jonaproitz@gmail.com>
2021-10-04 23:57:54 +0200

    Remove unnecesary methods and make variables public

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -3,24 +3,10 @@
 
 // Constructor
 Particle::Particle(int charge_in, double mass_in, arma::vec position_in, arma::vec velocity_in){
-    charge_ = charge_in;
-    mass_ = mass_in;
-    position_ = position_in;
-    velocity_ = velocity_in;
+    charge = charge_in;
+    mass = mass_in;
+    position = position_in;
+    velocity = velocity_in;
 }
 
-int Particle::charge(){
-    return charge_;
-}
-
-double Particle::mass(){
-    return mass_;
-}
-
-arma::vec Particle::position(){
-    return position_;
-}
-
-arma::vec Particle::velocity(){
-    return velocity_;
 }
