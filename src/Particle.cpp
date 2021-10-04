b3f31d060ebcc75c16839841909fcf18f6d8bd3e
jonaproitz <jonaproitz@gmail.com>
2021-10-04 15:43:37 +0200

    Store input as private variables

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -3,8 +3,8 @@
 
 // Constructor
 Particle::Particle(int charge_in, double mass_in, arma::vec position_in, arma::vec velocity_in){
-    int charge = charge_in;
-    double mass = mass_in;
-    arma::vec position = position_in;
-    arma::vec velocity = velocity_in;
+    int charge_ = charge_in;
+    double mass_ = mass_in;
+    arma::vec position_ = position_in;
+    arma::vec velocity_ = velocity_in;
 }
