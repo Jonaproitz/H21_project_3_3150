92b14f0f4c4a2b10acb81fee99cb0a36b4050366
jonaproitz <jonaproitz@gmail.com>
2021-10-04 23:56:59 +0200

    make member variables public

--- include/Particle.hpp
+++ include/Particle.hpp
@@ -5,28 +5,15 @@
 #define __Partilce_hpp__
 
 class Particle{
-private:
-    // Declaration of variables only accessible from within the class
-    int charge_;
-    double mass_;
-    arma::vec position_;
-    arma::vec velocity_;
-
 public:
+    // Declaration of public variables
+    int charge;
+    double mass;
+    arma::vec position;
+    arma::vec velocity;
+
     // Declaration of constructor
     Particle(int charge, double mass, arma::vec position, arma::vec velocity);
-
-    // Method that returns the charge of a particle
-    int charge();
-
-    // Method that returns the mass of a particle
-    double mass();
-
-    // Method that returns positoon of particle
-    arma::vec position();
-
-    // Method that returns velocity of particle
-    arma::vec velocity();
 };
 
 #endif
