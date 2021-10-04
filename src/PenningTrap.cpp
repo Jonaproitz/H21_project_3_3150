a976c53cc75a4e16f1f7dbdbe63529949b1047d4
jonaproitz <jonaproitz@gmail.com>
2021-10-04 14:07:13 +0200

    Store input

--- src/PenningTrap.cpp
+++ src/PenningTrap.cpp
@@ -3,7 +3,9 @@
 
 //Constructor
 PenningTrap::PenningTrap(double B0_in, double V0_in, double d_in){
-    
+    double B0 = B0_in;
+    double V0_in;
+    double d = d_in;
 }
 
 void PenningTrap::add_particle(Particle particles_in){
