964fb5235816fce36d010e25c38408b404095d8b
jonaproitz <jonaproitz@gmail.com>
2021-10-05 00:00:15 +0200

    Fix double declaration of member variables

--- src/PenningTrap.cpp
+++ src/PenningTrap.cpp
@@ -3,9 +3,9 @@
 
 //Constructor
 PenningTrap::PenningTrap(double B0_in, double V0_in, double d_in){
-    double B0_ = B0_in;
-    double V0_ = V0_in;
-    double d_ = d_in;
+    B0_ = B0_in;
+    V0_ = V0_in;
+    d_ = d_in;
 }
 
 void PenningTrap::add_particle(Particle particle_in){
