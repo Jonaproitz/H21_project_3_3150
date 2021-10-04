b88f5698dc0c3ecf91c1bf2550db2ca2d1a8ecd7
jonaproitz <jonaproitz@gmail.com>
2021-10-04 15:23:25 +0200

    Add expression for the external E field

--- src/PenningTrap.cpp
+++ src/PenningTrap.cpp
@@ -3,11 +3,19 @@
 
 //Constructor
 PenningTrap::PenningTrap(double B0_in, double V0_in, double d_in){
-    double B0 = B0_in;
-    double V0 = V0_in;
-    double d = d_in;
+    double B0_ = B0_in;
+    double V0_ = V0_in;
+    double d_ = d_in;
 }
 
 void PenningTrap::add_particle(Particle particle_in){
     particles.push_back(particle_in);
+}
+
+arma::vec PenningTrap::external_E_field(arma::vec r){
+    arma::vec E;
+    double x = r[0], y = r[1], z = r[2];
+    E = {-2*x, -2*y, 4*z};
+    E = V0_/(2.*d_*d_) * E;
+    return E;
 }
