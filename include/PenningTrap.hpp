253bdfa73912776d0b6fa1d59e364fc789530dd6
jonaproitz <jonaproitz@gmail.com>
2021-10-04 13:45:02 +0200

    Add total force on particles

--- include/PenningTrap.hpp
+++ include/PenningTrap.hpp
@@ -19,4 +19,7 @@ class PenningTrap{
 
         // Force on particle_i from particle_j
         arma::vec force_particle(int i, int j);
+
+        // The total force on particle_i from the external fields
+        arma::vec total_force_external(int i);
 };
