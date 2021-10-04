6707a585426c71012b1fb66a138ab6bf2e4fe309
jonaproitz <jonaproitz@gmail.com>
2021-10-04 13:38:43 +0200

    Add add_particle function

--- src/PenningTrap.cpp
+++ src/PenningTrap.cpp
@@ -2,6 +2,10 @@
 #include "PenningTrap.hpp"
 
 //Constructor
-PenningTrap::PenningTrap(double B0, double V0, double d, std::vector<Particle> particles){
+PenningTrap::PenningTrap(double B0, double V0, double d){
+
+}
+
+void PenningTrap::add_particle(std::vector<Particle> particles){
 
 }
