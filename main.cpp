89630494a49e8ad58d6b672f4025e444239ac146
jonaproitz <jonaproitz@gmail.com>
2021-10-04 16:09:14 +0200

    test that position returns position

--- main.cpp
+++ main.cpp
@@ -6,7 +6,10 @@ int main(){
     double B0 = 9.65e1, V0 = 9.65e8, d = 1e4;
     PenningTrap my_trap(B0, V0, d);
 
-
+    // Add particle
+    arma::vec pos = {1, 1, 1}, vel = {-1, -1, -1};
+    Particle particle1(1, 1, pos, vel);
+    std::cout << particle1.position() << std::endl;
 
     return 0;
 }
