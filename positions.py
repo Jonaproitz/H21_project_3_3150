9da56f7365ba5bdbb13272c8a25347cd5bb23116
jonaproitz <jonaproitz@gmail.com>
2021-10-16 23:34:43 +0200

    Plot z coordinates

--- positions.py
+++ positions.py
@@ -2,3 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pyarma as pa
 
+
+filename = "positions.bin"
+
+R = pa.mat()
+R.load(filename)
+
+x, y, z = np.array(R)
+
+plt.figure()
+
+plt.plot(z)
+
+plt.show()
