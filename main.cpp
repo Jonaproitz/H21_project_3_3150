4e6ebd4aae6dcd075a49e6c99be68b867f97138f
jonaproitz <jonaproitz@gmail.com>
2021-10-16 21:44:59 +0200

    Test integration methods

--- main.cpp
+++ main.cpp
@@ -10,9 +10,13 @@ int main(){
     arma::vec pos = {1, 1, 1}, vel = {-1, -1, -1};
     Particle particle_1(1, 1, pos, vel);
     my_trap.add_particle(particle_1);
+
+    my_trap.evolve_RK4(1e-4);
     my_trap.particles[0].velocity.print();
 
-    my_trap.evolve_forward_Euler(1e-2);
+    my_trap.particles[0].velocity = vel;
+    my_trap.particles[0].position = pos;
+    my_trap.evolve_forward_Euler(1e-4);
     my_trap.particles[0].velocity.print();
 
     return 0;
