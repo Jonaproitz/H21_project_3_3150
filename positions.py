917e6b19928ba8860ff897745af7f1cc84e3293f
jonaproitz <jonaproitz@gmail.com>
2021-10-17 20:58:03 +0200

    Plot x and y coordinates of 2 particles

--- positions.py
+++ positions.py
@@ -22,3 +22,14 @@ plt.plot(z)
 
 plt.show()
 """
+r = read_data("positions.txt")
+
+#print(r.shape)
+
+r1 = r[0]
+
+r2 = r[1]
+
+plt.plot(np.transpose(r1)[0], np.transpose(r1)[1])
+plt.plot(np.transpose(r2)[0], np.transpose(r2)[1])
+plt.show()
