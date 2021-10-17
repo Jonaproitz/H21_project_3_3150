5a06dcaeaa7a15379ee8a642b1df27aaae16895a
jonaproitz <jonaproitz@gmail.com>
2021-10-17 20:15:59 +0200

    Read postions from file

--- positions.py
+++ positions.py
@@ -1,10 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pyarma as pa
 
 
-r = np.loadtxt("positions.txt")
-x, y, z = np.transpose(r)
+def read_data(filename):
+    infile = np.loadtxt(filename)
+    N = int(np.max(infile.transpose()[0]) + 1)
+    index = np.where(infile.transpose()[0] == 0)
+    r = np.array([infile[index, 1:]])
+    for i in range(1, N):
+        index = np.where(infile.transpose()[0] == i)
+        r = np.append(r, [infile[index, 1:]], axis=0)
+    return r
+
+
+r = read_data("positions.txt")
+x, y, z = np.transpose(r[0])
 
 plt.figure()
 
