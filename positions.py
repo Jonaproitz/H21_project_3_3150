5e5762cc7a5540b9ef85d15b22df8678590e7be3
jonaproitz <jonaproitz@gmail.com>
2021-10-18 15:59:09 +0200

    Change array variable in read function

--- positions.py
+++ positions.py
@@ -6,11 +6,11 @@ def read_data(filename):
     infile = np.loadtxt(filename)
     N = int(np.max(infile.transpose()[0]) + 1)
     index = np.where(infile.transpose()[0] == 0)
-    r = np.array(infile[index, 1:])
+    x = np.array(infile[index, 1:])
     for i in range(1, N):
         index = np.where(infile.transpose()[0] == i)
-        r = np.append(r, infile[index, 1:], axis=0)
-    return r
+        x = np.append(x, infile[index, 1:], axis=0)
+    return x
 
 def plot_1_particle():
     r = read_data("positions.txt")
