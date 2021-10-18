507bb078fd33c9101c4286af83e9eb437307b0ad
jonaproitz <jonaproitz@gmail.com>
2021-10-18 16:05:52 +0200

    Remove commented code

--- main.cpp
+++ main.cpp
@@ -48,9 +48,7 @@ int main(){
     int width = 18;
     int prec = 10;
 
-    //ofile << "Number of particles\n" << n << std::endl;
     for (int t_i = 0; t_i < N; t_i++){
-        //ofile << "Time\n" << t[t_i] << std::endl;
         for (int i = 0; i < n; i++){
             ofile << i
                   << std::setw(width) << std::setprecision(prec) << std::scientific << r.slice(i).col(t_i)(0)
